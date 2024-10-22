6fc4980dee0d9252fabc7716a82ad7a708469f73
gillian-raab <gillian.raab@ed.ac.uk>
2024-10-22 18:38:39 +0100

    xxx

--- R/functions.syn.r
+++ R/functions.syn.r
@@ -903,9 +903,9 @@ syn.catall <- function(x, k, proper = FALSE, priorn = 1, structzero = NULL, maxt
 {
  # Fits a saturated model to combinations of variables
  # xp just holds number of synthetic records required
-  cat(noisetype,"noisetype\n\n")
- if (!(noisetype %in% c("Laplace","Gaussian", "Exponential",""))) stop("catall.noisetype must be one of  c('Laplace','Gaussian', 'Exponential','')",call. = FALSE)
- else if (noisetype %in% c("Laplace","Gaussian", "Exponential" )  && epsilon <= 0 )  stop("catall.epsilon to make catall DP must be >0",call. = FALSE)
+ if (!(noisetype %in% c("Laplace","Gaussian", "Exponential",""))) stop("catall.noisetype must be one of  c('Laplace','Gaussian', 'Exponential','')",epsilon, "epsilon" ,call. = FALSE)
+  
+#if (noisetype %in% c("Laplace","Gaussian", "Exponential" )  && epsilon <= 0 )  stop('catall.epsilon to make catall DP must be >0',call. = FALSE)
  if (noisetype %in% c("Laplace", "Exponential") ) cat("catall made DP with ",noisetype,"noise and epsilon set to", epsilon,"\n")
  if (noisetype == "Gaussian") cat("Gaussian noise added to make catall approximately DP with epsilon", epsilon," and delta", delta,"\n")
 
