f72cb913795deb133c86c31c7f2296da3fe73e16
gillian-raab <gillian.raab@ed.ac.uk>
2024-10-24 10:51:38 +0100

    zzz again

--- R/zzz.r
+++ R/zzz.r
@@ -1,5 +1,6 @@
 .onAttach <- function(...) {
-  info <- c("New version of synthpop with disclosure functions",
-            "Find out more at https://www.synthpop.org.uk/")
+  info <- c("New version of synthpop (1.9-0) with disclosure functions",
+            "see disclosure.pdf for details and NEWS file for other changes",
+           "","Find out more at https://www.synthpop.org.uk/")
     packageStartupMessage(paste(strwrap(info), collapse = "\n"))
 }
